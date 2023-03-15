e27ce5fbd982a8ac494b0b6dcb763802ab3e2a5d
natazuchowska <98778016+natazuchowska@users.noreply.github.com>
2023-03-15 12:34:25 +0000

    search bar

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '23fea07656msh70688f55bb421dbp1f1be4jsnf6595e024289',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";

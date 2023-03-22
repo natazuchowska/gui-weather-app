c26bfce954fbfc60e85aaf82408862cabfecc0f5
natazuchowska <98778016+natazuchowska@users.noreply.github.com>
2023-03-22 23:29:10 +0000

    profile pg initial layout hardcoded

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -6,8 +6,20 @@ function Profile(){
     return(
         <div className='profile-section'>
             <header className="title">Jane Reid</header>
+            <p className='subscr'>Farmer</p>
             <div>
                 <img alt="farmer" className='profile-img' src="./icons/profile.png"></img>
+            </div>
+            <div className='about-me'>
+                <h1>About me</h1>
+                <p>blablablabla</p>
+                <h1>Crops I like to grow</h1>
+                <list>
+                    <li>crop 1</li>
+                    <li>crop 2</li>
+                    <li>crop 3</li>
+                    <li>crop 4</li>
+                </list>
             </div>
         </div>
     )
